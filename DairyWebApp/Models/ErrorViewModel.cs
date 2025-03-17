272b313db428067a4bab2a48941ad71ec8d08438
Aditya Pachupate <adityapachupate@gmail.com>
2025-03-17 22:12:20 +0530

    Add project files.

---
+++ DairyWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DairyWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
